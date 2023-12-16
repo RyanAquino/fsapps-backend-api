41c207c460af9dc356ac6243943d86959d5aad65
Walter <walterangelodasmarinas@gmail.com>
2023-12-16 12:13:09 +0800

    Removed the sample database data and connected the real database data from mariadb. Removed the secret key and the algorithm and add it to the environment variables. Add the code in accessing the secret key and algorithm from the environment variables. Fixed the function which authenticates the user from the sample database to the connected database. Add new packages to the requirements.txt. Fixed the sql statements in fetching a specific user from the database.

--- main.py
+++ main.py
@@ -4,19 +4,15 @@ from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from schema import (
+    get_registered_user,
+    init_db
+)
 import os
 
-SECRET_KEY = "8837fccb8a19b9a6e942bee4ccc78c6c9791c60f21ee1391219c9d233af763bb"
-ALGORITHM = "HS256"
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret")
+ALGORITHM = os.environ.get("ALGORITHM", "algorithm")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-db = {
-    "walter": {
-        "username": "gelo",
-        "hashed_password": "",
-        "enabled": False
-    }
-}
 
 
 class Token(BaseModel):
@@ -54,20 +50,21 @@ def get_password_hash(password):
 
 
 # Get user in database using the username
-def get_user(db, username: str):
-    if username in db:
-        user_data = db[username]
-        return UserInDB(**user_data)
+def get_user(username: str):
+    user_db_data = get_registered_user(init_db(), username)
+    if user_db_data is not None:
+        return UserInDB(username=user_db_data["username"],
+                        hashed_password=user_db_data["hashed_password"],
+                        enabled=user_db_data["enabled"])
 
 
 # Checks the given password and username if it is registered in the database
-def authenticate_user(db, username: str, password: str):
-    user = get_user(db, username)
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
-
     return user
 
 
@@ -80,7 +77,7 @@ def create_access_token(data: dict, expires_delta: timedelta or None = None):
         expire = datetime.utcnow() + timedelta(minutes=15)
 
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
     return encoded_jwt
 
 
@@ -99,7 +96,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise credential_exception
 
-    user = get_user(db, username=token_data.username)
+    user = get_user(username=token_data.username)
     if user is None:
         raise credential_exception
 
@@ -110,14 +107,13 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
     if not current_user.enabled:
         raise HTTPException(status_code=400, detail="Inactive user")
-
     return current_user
 
 
 # Path used in creating the access token of the logged-in user
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(db, form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail="Incorrect username or password",
@@ -138,3 +134,10 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 @app.get("/user/me/items")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": 1, "owner": current_user}]
+
+
+# starting code in registering user import register user from schema
+# @app.post("/registering_user"):
+# async def registering_user():
+#     register_user(init_db(), username, hashed_password)
+#     return "Registered"
--- schema.py
+++ schema.py
@@ -1,41 +1,60 @@
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 import os
 import mariadb
 
 
-def create_database(db_cursor):
+# Converting array to dictionary
+def convert_array(keys, values):
+    dictionary = {keys[i]: values[i] for i in range(len(keys))}
+    return dictionary
+
+
+# Create the required database
+def create_database(db_engine):
     sql_statement = "CREATE DATABASE IF NOT EXISTS users_information"
-    db_cursor.execute(sql_statement)
+    db_engine["db_cursor"].execute(sql_statement)
     print("DATABASE CREATED")
 
 
-def create_registered_users_table(db_cursor):
+# Create the required table for registered_users
+def create_registered_users_table(db_engine):
     sql_statement = ("CREATE TABLE IF NOT EXISTS registered_users "
                      "(id INT NOT NULL AUTO_INCREMENT, "
                      "username VARCHAR(100) NOT NULL,"
                      "hashed_password VARCHAR(100) NOT NULL,"
                      "enabled TINYINT NOT NULL,"
                      "PRIMARY KEY(id))")
-    db_cursor.execute(sql_statement)
+    db_engine["db_cursor"].execute(sql_statement)
     print("REGISTERED USERS TABLE CREATED")
 
 
-def register_user(db_cursor, username: str, hashed_password: str):
+# Add new user into the registered_users table
+def register_user(db_engine, username: str, hashed_password: str):
     sql_statement = (f"INSERT INTO registered_users (username, hashed_password, enabled) "
-                     f"VALUES ('{username}', '{hashed_password}', '{1}')")
-    db_cursor.execute(sql_statement)
+                     f"VALUES (%s,%s,%s)")
+    db_engine["db_cursor"].execute(sql_statement, (username, hashed_password, 1))
+    db_engine["db_connection"].commit()
+    return_statement = username + " has been registered"
+    return return_statement
 
 
-def get_registered_user(db_cursor, username: str):
+# Get a specific user in the registered_users table
+def get_registered_user(db_engine, username: str):
     sql_statement = (f"SELECT username, hashed_password, enabled "
-                     f"from registered_users where username={username}")
-    return db_cursor.execute(sql_statement)
+                     f"from registered_users where username=%s")
+    columns = ["username", "hashed_password", "enabled"]
+    db_engine["db_cursor"].execute(sql_statement, (username, ))
+    registered_user = db_engine["db_cursor"].fetchone()
+    registered_user_dict = convert_array(columns, registered_user)
+    return registered_user_dict
 
 
+# Load the .env file
 def configure():
     load_dotenv()
 
 
+# Initialize the database
 def init_db():
     configure()
     try:
@@ -46,11 +65,7 @@ def init_db():
             database=os.environ.get("DATABASE", "users_information")
         )
         db_cursor = conn.cursor()
-        return db_cursor
+        db_engine = {"db_connection": conn, "db_cursor": db_cursor}
+        return db_engine
     except mariadb.Error as e:
         print(f"Error: {e}")
-
-
-#cursor = init_db()
-#register_user(cursor, "admin", "$2b$12$NKg.483CpDHY1mis.tyKRe.bxTfUvrBea1SEFHeQAjDr7wcufSDYa")
-#register_user(cursor, "user", "pass")
