cf88ba93dbe2878472ffc4feb436fc7f895a5162
Walter <walterangelodasmarinas@gmail.com>
2023-12-19 16:53:48 +0800

    Add requests, datetime, jose packages. Fixed the tokenUrl. Fixed the functions in authenticating users. Fixed the function in encoding and decoding JWT tokens. Add request package in requirements.txt. Deleted schema.py Change the datatype of enabled column in UserBase model.

--- api/crud/crud.py
+++ api/crud/crud.py
@@ -17,7 +17,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate, hashed_password: str):
-    db_user = models.User(email=user.email, hashed_password=hashed_password, is_enabled=True)
+    db_user = models.User(username=user.username, hashed_password=hashed_password, is_enabled=True)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
