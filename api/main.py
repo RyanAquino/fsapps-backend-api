2be455f5e83bb6c430d2e559150f13d2fe6c8c58
Walter <walterangelodasmarinas@gmail.com>
2023-12-19 17:02:57 +0800

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,12 +1,15 @@
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+import uvicorn
+import requests
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from functools import lru_cache
 
-from database import SessionLocal, engine
+from api.database import SessionLocal
 from crud import crud
-from models import models
 from schemas import schemas
 from api import config
 
@@ -21,9 +24,8 @@ SECRET_KEY = settings.secret_key
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/auth/login")
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
@@ -46,91 +48,84 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-# # Get user in database using the username
-# def get_user(username: str):
-#     user_db_data = get_registered_user(init_db(), username)
-#     if user_db_data is not None:
-#         return UserInDB(username=user_db_data["username"],
-#                         hashed_password=user_db_data["hashed_password"],
-#                         enabled=user_db_data["enabled"])
+# Get request user if available in the database
+def get_request_user(username):
+    base_url = "http://localhost:8000"
+    endpoint = "users"
+    request_user = requests.get(f"{base_url}/{endpoint}/{username}", timeout=20)
+    if request_user.status_code == 200:
+        return request_user.json()
 
 
-# # Checks the given password and username if it is registered in the database
-# def authenticate_user(username: str, password: str):
-#     user = get_user(username)
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
-#
-#
-# # Create JWT token with an expiry based on the given data
-# def create_access_token(data: dict, expires_delta: timedelta or None = None):
-#     to_encode = data.copy()
-#     if expires_delta:
-#         expire = datetime.utcnow() + expires_delta
-#     else:
-#         expire = datetime.utcnow() + timedelta(minutes=15)
-#
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
-#     return encoded_jwt
-#
-#
-# # Check the given token if the details are matched in the database
-# async def get_current_user(token: str = Depends(oauth2_scheme)):
-#     credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-#                                          detail="Could not validate credentials",
-#                                          headers={"WWW-Authenticate": "Bearer"})
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credential_exception
-#
-#         token_data = TokenData(username=username)
-#     except JWTError:
-#         raise credential_exception
-#
-#     user = get_user(username=token_data.username)
-#     if user is None:
-#         raise credential_exception
-#
-#     return user
-#
-#
-# # Check if the user token is expired or not
-# async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
-#     if not current_user.enabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
-#
-#
-# # Path used in creating the access token of the logged-in user
-# @app.post("/v1/auth/login", response_model=Token)
-# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-#     user = authenticate_user(form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-#                             detail="Incorrect username or password",
-#                             headers={"WWW-Authenticate": "Bearer"})
-#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = create_access_token(data={"sub": user.username},
-#                                        expires_delta=access_token_expires)
-#     return {"access_token": access_token, "token_type": "bearer"}
-#
-#
-# # Sample path in checking if the token works
-# @app.get("/users/me/")
-# async def read_users_me(current_user: User = Depends(get_current_active_user)):
-#     return current_user
-#
-#
-# # Sample path in returning json format data
-# @app.get("/user/me/items")
-# async def read_own_items(current_user: User = Depends(get_current_active_user)):
-#     return [{"item_id": 1, "owner": current_user}]
+# Checks the given password and username if it is registered in the database
+def authenticate_user(username: str, password: str):
+    user = get_request_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user["hashed_password"]):
+        return False
+    return user
+
+
+# Create JWT token with an expiry based on the given data
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm="HS256")
+    return encoded_jwt
+
+
+# Check the given token if the details are matched in the database
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                         detail="Could not validate credentials",
+                                         headers={"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=["HS256"])
+        username: str = payload.get("username")
+        if username is None:
+            raise credential_exception
+
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credential_exception
+
+    user = get_request_user(username=token_data.username)
+    if user is None:
+        raise credential_exception
+
+    return user
+
+
+# Check if the user token is expired or not
+def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
+    if not current_user.enabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+# Path used in creating the access token of the logged-in user
+@app.post("/v1/auth/login", response_model=schemas.Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username or password",
+                            headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(user, settings.access_token_minutes)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+# Sample path in checking if the token works
+@app.get("/userstest/me/")
+def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
+    return current_user
 
 
 # Creating a user
@@ -151,7 +146,7 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 
 # Get a user using their username
-@app.get("/users/{user_name}", response_model=schemas.User)
+@app.get("/users/{username}", response_model=schemas.User)
 def read_user(username: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=username)
     if db_user is None:
@@ -159,8 +154,7 @@ def read_user(username: str, db: Session = Depends(get_db)):
     return db_user
 
 
-# starting code in registering user import register user from schema
-# @app.post("/registering_user"):
-# async def registering_user():
-#     register_user(init_db(), username, hashed_password)
-#     return "Registered"
+if __name__ == "__main__":
+    uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
+
+
