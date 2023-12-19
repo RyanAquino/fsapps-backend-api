63430bbe79ff47c27955b3d40bbeef5f0e18434d
Walter <walterangelodasmarinas@gmail.com>
2023-12-19 17:24:49 +0800

    Update config.py, main.py, models.py based on branch tsk-43-r1.
    Deleted schema.py.

--- api/models/models.py
+++ api/models/models.py
@@ -1,13 +1,16 @@
 from sqlalchemy import Boolean, Column, Integer, String
 
-from api.database import Base
+from api.database import Base, engine
 
 
 class User(Base):
     __table_args__ = {'extend_existing': True}
     __tablename__ = "registered_users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(100), unique=True, index=True)
     hashed_password = Column(String(100), nullable=False)
     is_enabled = Column(Boolean, default=True)
+
+
+Base.metadata.create_all(engine, checkfirst=True)
